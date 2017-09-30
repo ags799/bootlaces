b8bf61709cdbeaefd6e426d97be51c552e9372d5
Andrew Sharp <andrew.sharp57@gmail.com>
2017-09-29 22:24:57 -0500

    using version from code

--- src/ags799/bootlaces.clj
+++ src/ags799/bootlaces.clj
@@ -1,5 +1,17 @@
-(ns ags799.bootlaces (:gen-class))
+(ns ags799.bootlaces
+  (:gen-class)
+  (:require [clojure.java.shell :refer [sh]]))
 
 (defn greeting [] "Hello, world!")
+
+(defn version
+  "Returns the project's version.
+
+  The version is the HEAD commit's short hash. This is done to support
+  continuous deployment: if we deploy every commit on the deployed branch,
+  then we won't necessarily tag every commit that is deployed. However, those
+  commits will have a hash. That's what we use as our version."
+  []
+  (clojure.string/trim (:out (sh "git" "rev-parse" "--short" "HEAD"))))
 
 (defn -main [] (println (greeting)))
